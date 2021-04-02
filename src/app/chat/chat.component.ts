4ba27aa4d27260c37dea4c5f060a2db339b95e7d
Marcus Chok <marcus.chok110@gmail.com>
2021-04-02 16:36:03 -0400

    Create basic chat card with input group

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Message, mockMessages } from './shared/message.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
+  public messages: Message[] = [];
+  public messageInput = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  private fetchMessages(): void {
+    this.http.get<Message[]>(environment.serverUrl).subscribe(
+      (messages) => {
+        this.messages = messages;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  public onSubmit(): void {
+    console.log(this.messageInput);
+    this.messageInput = '';
+  }
+
+  public loadMockMessages(): void {
+    this.messages = mockMessages;
+  }
+
+  ngOnInit(): void {
+    this.fetchMessages();
+  }
 }

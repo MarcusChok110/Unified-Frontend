9a06c038e7fdeae9cf3e9fa03b9d3e6089a02a01
Marcus Chok <marcus.chok110@gmail.com>
2021-04-02 16:34:33 -0400

    Create Message model to match model on backend

---
+++ src/app/chat/shared/message.model.ts
@@ -0,0 +1,24 @@
+export class Message {
+  id?: number;
+  date?: Date;
+
+  constructor(
+    public authorId: string,
+    public author: string,
+    public content: string,
+    date?: Date
+  ) {
+    this.date = date;
+  }
+}
+
+export const mockMessages = [
+  new Message('12390123123', 'someone', 'once told me'),
+  new Message(
+    'auth0|182390-192sxj',
+    'the world',
+    'was gonna roll me',
+    new Date()
+  ),
+  new Message('auth0|182390-192sxj', 'help', 'idk what im doing'),
+];

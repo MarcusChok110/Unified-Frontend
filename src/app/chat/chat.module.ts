7b29a8edb37aebcc53778625ebf771bc482fc325
Marcus Chok <marcus.chok110@gmail.com>
2021-04-02 16:35:03 -0400

    Add FormsModule and HttpClientModule in imports

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './chat.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ChatComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule],
   exports: [ChatComponent],
 })
 export class ChatModule {}

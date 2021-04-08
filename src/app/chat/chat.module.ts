53daee67b5c353aa523fbfe130921f1c678bb42d
Marcus Chok <marcus.chok110@gmail.com>
2021-04-08 17:37:46 -0400

    Create chat module and update imports

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ChatComponent } from './chat.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MessageCardComponent } from './message-card/message-card.component';
+import { MessageService } from './shared/message.service';
+import { ProfileModule } from '../profile/profile.module';
 
 @NgModule({
-  declarations: [ChatComponent],
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule],
+  declarations: [ChatComponent, MessageCardComponent],
+  providers: [MessageService],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ProfileModule,
+  ],
   exports: [ChatComponent],
 })
 export class ChatModule {}

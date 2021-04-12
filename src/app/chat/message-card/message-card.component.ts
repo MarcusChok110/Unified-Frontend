123d1eedb3b00e955c9247ede5135b1c3940588e
Marcus Chok <marcus.chok110@gmail.com>
2021-04-12 14:03:58 -0400

    Add button and binding for deleting messages to message card component

--- src/app/chat/message-card/message-card.component.ts
+++ src/app/chat/message-card/message-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Message } from '../shared/message.model';
 
 @Component({
@@ -9,8 +9,13 @@ import { Message } from '../shared/message.model';
 export class MessageCardComponent implements OnInit {
   @Input() message!: Message;
   @Input() userId?: string;
+  @Output() deleteEvent = new EventEmitter<number>();
 
   constructor() {}
+
+  public onDelete(): void {
+    this.deleteEvent.emit(this.message.id);
+  }
 
   ngOnInit(): void {
     // validate that props are present
